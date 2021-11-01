795011541c900d570333791487b957a18fcb6cb9
fajar7xx <fajar7xx@gmail.com>
2021-11-01 23:45:22 +0700

    mantap

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="movie-info border-b border-gray-100">
-        <div class="container mx-auto px-4 py-16 flex">
-            <img src="{{asset('img/parasite.jpg')}}" alt="parasite" class="w-96">
-            <div class="ml-24">
+        <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
+            <img src="{{asset('img/parasite.jpg')}}" alt="parasite" class="w-64 md:w-96">
+            <div class="md:ml-24">
                 <h2 class="text-4xl font-semibold">Parasite (2019)</h2>
-                <div class="flex items-center text-gray-400 text-sm">
+                <div class="flex flex-wrap items-center text-gray-400 text-sm">
                     <svg class="fill-current text-orange-500 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.94 47.94" style="enable-background:new 0 0 47.94 47.94" xml:space="preserve"><path style="fill:#ed8a19" d="m26.285 2.486 5.407 10.956a2.58 2.58 0 0 0 1.944 1.412l12.091 1.757c2.118.308 2.963 2.91 1.431 4.403l-8.749 8.528a2.582 2.582 0 0 0-.742 2.285l2.065 12.042c.362 2.109-1.852 3.717-3.746 2.722l-10.814-5.685a2.585 2.585 0 0 0-2.403 0l-10.814 5.685c-1.894.996-4.108-.613-3.746-2.722l2.065-12.042a2.582 2.582 0 0 0-.742-2.285L.783 21.014c-1.532-1.494-.687-4.096 1.431-4.403l12.091-1.757a2.58 2.58 0 0 0 1.944-1.412l5.407-10.956c.946-1.919 3.682-1.919 4.629 0z"/></svg>
                     <span class="ml-1">85%</span>
                     <span class="mx-2">|</span>
@@ -26,8 +26,49 @@
                     inventore ipsa iste iusto nihil, nobis omnis provident
                     quas recusandae reiciendis rem saepe sed similique soluta voluptates?
                 </p>
+
+                <div class="mt-12">
+                    <h4 class="text-white font-semibold">Featured Cast</h4>
+                    <div class="flex mt-4">
+                        <div class="">
+                            <div>Bong Joon Hoo</div>
+                            <div class="text-sm text-gray-400">Screenplay, Director, Story</div>
+                        </div>
+                        <div class="ml-8">
+                            <div>Han Jin-Won</div>
+                            <div class="text-sm text-gray-400">Screenplay</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-12">
+                    <button class="flex items-center bg-yellow-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-yellow-600 transition ease-in-out duration-150">
+                        <svg class="w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 485 485" style="enable-background:new 0 0 485 485" xml:space="preserve"><path d="M413.974 71.026C368.171 25.225 307.274 0 242.5 0S116.829 25.225 71.026 71.026C25.225 116.829 0 177.726 0 242.5s25.225 125.671 71.026 171.474C116.829 459.775 177.726 485 242.5 485s125.671-25.225 171.474-71.026C459.775 368.171 485 307.274 485 242.5s-25.225-125.671-71.026-171.474zM242.5 455C125.327 455 30 359.673 30 242.5S125.327 30 242.5 30 455 125.327 455 242.5 359.673 455 242.5 455z"/><path d="M181.062 336.575 343.938 242.5l-162.876-94.075z"/></svg>
+                        <span class="ml-2">Play Trailer</span>
+                    </button>
+                </div>
             </div>
         </div>
+    </div>
 
+    <div class="movie-cast border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">Cast</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                @for($i=0; $i<5; $i++)
+                    <div class="mt-8">
+                        <a href="#">
+                            <img src="{{asset('img/actor1.jpg')}}" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="mt-2">
+                            <a href="#" class="text-lg mt-2 hover:text-gray-300">John Smith</a>
+                            <div class="text-gray-400 text-sm">
+                                Main Actor
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
     </div>
 @endsection
